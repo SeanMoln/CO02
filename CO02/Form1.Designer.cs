40f5da25911ed68074d29cf1a5eecb292976f052
Shute <Shute@>
2019-03-04 14:34:42 +0800

    加入專案檔案。

---
+++ CO02/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace CO02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.LABEL1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dotdot = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LOAD = new System.Windows.Forms.Button();
+            this.OUT = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(662, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // LABEL1
+            // 
+            this.LABEL1.AutoSize = true;
+            this.LABEL1.Location = new System.Drawing.Point(13, 15);
+            this.LABEL1.Name = "LABEL1";
+            this.LABEL1.Size = new System.Drawing.Size(56, 12);
+            this.LABEL1.TabIndex = 1;
+            this.LABEL1.Text = "資料來源:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(571, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dotdot
+            // 
+            this.dotdot.Location = new System.Drawing.Point(652, 10);
+            this.dotdot.Name = "dotdot";
+            this.dotdot.Size = new System.Drawing.Size(24, 23);
+            this.dotdot.TabIndex = 3;
+            this.dotdot.Text = "...";
+            this.dotdot.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "選擇工作表:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(109, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(283, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // LOAD
+            // 
+            this.LOAD.Location = new System.Drawing.Point(418, 87);
+            this.LOAD.Name = "LOAD";
+            this.LOAD.Size = new System.Drawing.Size(75, 23);
+            this.LOAD.TabIndex = 6;
+            this.LOAD.Text = "讀取";
+            this.LOAD.UseVisualStyleBackColor = true;
+            this.LOAD.Click += new System.EventHandler(this.LOAD_Click);
+            // 
+            // OUT
+            // 
+            this.OUT.Location = new System.Drawing.Point(515, 87);
+            this.OUT.Name = "OUT";
+            this.OUT.Size = new System.Drawing.Size(75, 23);
+            this.OUT.TabIndex = 7;
+            this.OUT.Text = "匯出手冊";
+            this.OUT.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 450);
+            this.Controls.Add(this.OUT);
+            this.Controls.Add(this.LOAD);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dotdot);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LABEL1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label LABEL1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button dotdot;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button LOAD;
+        private System.Windows.Forms.Button OUT;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
